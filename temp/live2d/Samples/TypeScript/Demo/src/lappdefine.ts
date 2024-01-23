7b98ef9923eb17cb0afa1c92c477d41fd9471dac
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2024-01-23 02:48:10 +0000

    set

--- temp/live2d/Samples/TypeScript/Demo/src/lappdefine.ts
+++ temp/live2d/Samples/TypeScript/Demo/src/lappdefine.ts
@@ -17,7 +17,7 @@ export const CanvasSize: { width: number; height: number } | 'auto' = 'auto';
 // 画面
 export const ViewScale = 1.0;
 export const ViewMaxScale = 2.0;
-export const ViewMinScale = 0.8;
+export const ViewMinScale = 0.1;
 
 export const ViewLogicalLeft = -1.0;
 export const ViewLogicalRight = 1.0;
