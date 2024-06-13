7ecfa9a362f6d442f827efd1b258f71b17f3a486
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2024-06-13 10:51:00 +0000

    set

--- app.js
+++
@@ -1,9 +0,0 @@
-if (navigator.serviceWorker.controller) {
-console.log('[PWA Builder] active service worker found, no need to register')
-} else {
-navigator.serviceWorker.register('/sw.js', {
-scope: 'https://si-kuma.f5.si'
-}).then(function(reg) {
-console.log('Service worker has been registered for scope:'+ reg.scope);
-});
-}
