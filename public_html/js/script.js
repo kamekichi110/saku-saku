3967c1e6534754972166c67ff0c8c71e1a66027f
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2024-06-14 00:52:55 +0000

    set

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -2,3 +2,15 @@
 function top() {
     window.location.href = "./";
 }
+function loadJson(url) {
+    return fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  }
--- public_html/sw-register.js
+++ public_html/sw-register.js
@@ -24,19 +24,6 @@ if ("serviceWorker" in navigator) {
     });
   }
 
-  function loadJson(url) {
-    return fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-      });
-  }
-
 function checkVer() {
     var jsonDataInfo;
     loadJson('./now-version.json').then(data => {
